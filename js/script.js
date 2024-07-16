1db56017a10c5f95206cda502510ff451b0d1aac
mancha-heinz <manchaheinz@gmail.com>
2024-07-15 21:26:48 -0300

    project setup

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log("load .js");
+
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
