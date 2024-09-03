cecc1665e0b82b4db5cc5d78b643fcfea3f13949
mancha-heinz <manchaheinz@gmail.com>
2024-09-03 19:38:31 -0300

    player sprite - attack

--- js/script.js
+++ js/script.js
@@ -66,6 +66,10 @@ const player = new Fighter({
       imageSrc: "./img/samuraiMack/Fall.png",
       framesMax: 2,
     },
+    attack1: {
+      imageSrc: "./img/samuraiMack/Aattack1.png",
+      framesMax: 6,
+    },
   },
 });
 
