ab0f2019bacf96035a821b8d6eb68e6657088733
mancha-heinz <manchaheinz@gmail.com>
2024-09-03 19:50:17 -0300

    enemy sprites - idle, run, jump, fall and attack

--- js/script.js
+++ js/script.js
@@ -67,7 +67,7 @@ const player = new Fighter({
       framesMax: 2,
     },
     attack1: {
-      imageSrc: "./img/samuraiMack/Aattack1.png",
+      imageSrc: "./img/samuraiMack/Attack1.png",
       framesMax: 6,
     },
   },
@@ -87,6 +87,35 @@ const enemy = new Fighter({
   offset: {
     x: -50,
     y: 0,
+  },
+  imageSrc: "./img/kenji/Idle.png",
+  framesMax: 4,
+  scale: 2.5,
+  offset: {
+    x: 215,
+    y: 167,
+  },
+  sprites: {
+    idle: {
+      imageSrc: "./img/kenji/Idle.png",
+      frames: 4,
+    },
+    run: {
+      imageSrc: "./img/kenji/Run.png",
+      frames: 8,
+    },
+    jump: {
+      imageSrc: "./img/kenji/Jump.png",
+      framesMax: 2,
+    },
+    fall: {
+      imageSrc: "./img/kenji/Fall.png",
+      framesMax: 2,
+    },
+    attack1: {
+      imageSrc: "./img/kenji/Attack1.png",
+      framesMax: 4,
+    },
   },
 });
 
@@ -139,8 +168,18 @@ function animate() {
   // enemy movement
   if (keys.ArrowLeft.pressed && enemy.lastKey === "ArrowLeft") {
     enemy.velocity.x = -5;
+    enemy.switchSprite("run");
   } else if (keys.ArrowRight.pressed && enemy.lastKey === "ArrowRight") {
     enemy.velocity.x = 5;
+    enemy.switchSprite("run");
+  } else {
+    enemy.switchSprite("idle");
+  }
+  // jumping
+  if (enemy.velocity.y < 0) {
+    enemy.switchSprite("jump");
+  } else if (enemy.velocity.y > 0) {
+    enemy.switchSprite("fall");
   }
   // player colisao
   if (
@@ -207,8 +246,7 @@ window.addEventListener("keydown", (event) => {
       enemy.velocity.y = -20;
       break;
     case "ArrowDown":
-      enemy.isAttacking = true;
-      // enemy.attack();
+      enemy.attack();
       break;
   }
 });
